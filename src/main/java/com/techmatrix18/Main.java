4af1b436a6228212c08e5ae045fa1d61080c8455
Alexander <alexander@makklays.com.ua>
2026-01-06 22:23:32 +0100

    06-01-2026 Add new Main.java

--- src/main/java/com/techmatrix18/Main.java
+++ src/main/java/com/techmatrix18/Main.java
@@ -1,5 +1,14 @@
-package com.techmatrix18;public class Main {
+package com.techmatrix18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello payments microservice!");
+
+        SpringApplication.run(Main.class, args);
     }
 }
+
